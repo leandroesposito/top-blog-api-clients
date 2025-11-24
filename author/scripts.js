342b124a37e7051d693ad3745aeb2c796eafab3f
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-11-23 21:52:54 -0300

    Use render service

--- author/scripts.js
+++ author/scripts.js
@@ -21,27 +21,7 @@ async function loadPosts() {
 
 function renderPost(post) {
   const postsContainer = document.querySelector(".posts");
-  const newPost = document.createElement("div");
-  newPost.classList.add("post");
-
-  const title = document.createElement("div", {});
-  title.classList.add("post-title");
-  title.textContent = post.title;
-  newPost.appendChild(title);
-
-  const content = document.createElement("div", {});
-  content.classList.add("post-content");
-  content.textContent = post.content;
-  newPost.appendChild(content);
-
-  const date = document.createElement("div", {});
-  date.classList.add("post-date");
-  date.textContent = post.date;
-  newPost.appendChild(date);
-
-  const comments = document.createElement("div", {});
-  comments.classList.add("post-comments");
-  newPost.appendChild(comments);
+  const newPost = RenderService.createPostElem(post);
 
   postsContainer.appendChild(newPost);
 }
