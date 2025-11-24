790e28fc6a4e1811942158105cc7d00217918ae3
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-11-24 19:56:56 -0300

    Implement new post handlers

--- author/scripts.js
+++ author/scripts.js
@@ -33,6 +33,65 @@ function renderPost(post) {
   postsContainer.appendChild(newPost);
 }
 
+function resetPostForm() {
+  const postForm = document.querySelector(".post-form");
+  const idInput = postForm.querySelector("[name='id']");
+
+  postForm.reset();
+  idInput.value = "";
+}
+
+function handleNewPostButton() {
+  resetPostForm();
+  const postFormContainer = document.querySelector(".post-form-container");
+  const titleInput = postFormContainer.querySelector("[name='title']");
+  const formTitle = postFormContainer.querySelector("h3");
+  formTitle.textContent = "New Post";
+
+  postFormContainer.classList.remove("hidden");
+  titleInput.focus();
+}
+
+function hidePostForm() {
+  resetPostForm();
+  const postFormContainer = document.querySelector(".post-form-container");
+  postFormContainer.classList.add("hidden");
+}
+
+async function handlePostSubmit() {
+  const postForm = document.querySelector(".post-form");
+  const formData = new FormData(postForm);
+
+  const post = {
+    title: formData.get("title"),
+    content: formData.get("content"),
+    "is-published": !!formData.get("is-published"),
+    id: formData.get("id"),
+  };
+
+  let result;
+
+  RenderService.showLoading();
+  if (post.id === "") {
+    result = await ApiService.createPost(post);
+  } else {
+    alert("NOT IMPLEMENTED");
+  }
+  RenderService.hideLoading();
+
+  if (result.errors) {
+    RenderService.displayErrors(result.errors);
+  }
+  if (result.success) {
+    RenderService.displaySuccess(result.success);
+    hidePostForm();
+  }
+
+  if (result.post) {
+    renderPost(result.post);
+  }
+}
+
 async function init() {
   if (!authService.isLogedIn()) {
     return (window.location.href = "./log-in.html");
@@ -41,6 +100,12 @@ async function init() {
 
   const logOutButton = document.querySelector(".log-out-button");
   logOutButton.addEventListener("click", handleLogOut);
+
+  const newPostButton = document.querySelector(".new-post-button");
+  newPostButton.addEventListener("click", handleNewPostButton);
+
+  const submitPostButton = document.querySelector(".post-form button");
+  submitPostButton.addEventListener("click", handlePostSubmit);
 
   await loadPosts();
 }
